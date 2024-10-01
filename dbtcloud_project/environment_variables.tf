c024b21c47d2666d81fad8bdd7516abcfcc1c1d3
katieclaiborne7 <39864736+katieclaiborne7@users.noreply.github.com>
2024-10-01 05:33:23 -0400

    remove project env var (#2)

--- dbtcloud_project/environment_variables.tf
+++ dbtcloud_project/environment_variables.tf
@@ -1,17 +1,3 @@
-resource "dbtcloud_environment_variable" "project" {
-  name       = "DBT_${upper(var.project_name)}_PROJECT"
-  project_id = dbtcloud_project.main.id
-  environment_values = {
-    "project" : "${local.gcp_project_root}-dev",
-    "Production" : "${local.gcp_project_root}-production",
-    "Staging" : "${local.gcp_project_root}-staging",
-  }
-  depends_on = [
-    dbtcloud_project.main,
-    dbtcloud_environment.deployment,
-  ]
-}
-
 resource "dbtcloud_environment_variable" "project_evaluator_enabled" {
   name       = "DBT_PROJECT_EVALUATOR_ENABLED"
   project_id = dbtcloud_project.main.id
